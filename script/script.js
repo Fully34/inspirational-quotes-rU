278c3d96c9745d2d726f9e2ae01416ec60f7122c
Fully34 <cfullinw34@gmail.com>
2015-07-07 14:57:42 -0600

    working on quote structure generator fn in jquery

--- script/script.js
+++ script/script.js
@@ -1,4 +1,23 @@
 $(function(){
 
-    alert('jquery active!')
+
+    var elements = function(){ 
+
+        return {
+            liC  : $('<li class="quote-container"></li>');
+            ul   : $('<ul class="quote-content"></ul>');
+            h3   : $('<h3 class="quote-title"></h3>');
+            p    : $('<p class="rating"></p>');
+            liQ  : $('<li class="quote-text"></li')
+            liA  : $('<li class="quote-author"></div>')
+        }
+    };
+
+    var quoteItem = function(){
+
+        var newEls = elements();
+
+        var ul = newEls.ul
+    }
+
 })
