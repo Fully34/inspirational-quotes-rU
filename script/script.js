3ee0b494212832a289b2a83eb1de8598bc11f542
Fully34 <cfullinw34@gmail.com>
2015-07-08 15:20:54 -0600

    working on dynamic content

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
     
-// $(function(){
+$(function(){
 
 
     //============================== Setting undo Variable ==============================//
             
-    var undo; // -> this only works for one deep undo
 
     //===========================================================================//
                             /* ~~~ Create quote structure ~~~ */ 
@@ -16,9 +15,9 @@
 
         var els =  {
 
+            del  : $('<div class="btn delete-btn"></div>'),
             liC  : $('<li class="quote-container"></li>'),
             ul   : $('<ul class="quote-content"></ul>'),
-            h3   : $('<h3 class="quote-title"></h3>'),
             p    : $('<p class="rating"></p>'),
             liQ  : $('<li class="quote-text"></li'),
             liA  : $('<li class="quote-author"></div>'),
@@ -27,22 +26,32 @@
         return els;
     };
 
+    var undo; // -> this only works for one deep undo
+
     // generate structure here
-    var quoteItem = function(){
+    var quoteItem = function(qText, qAuthor){
 
         // fresh batch of elements
         var newEls = elements();
 
         // new vars so we don't have to use dot notation all over the place -> could get messy since we will be chaining a lot of methods
+        var del = newEls.del
         var ul = newEls.ul;
         var liC = newEls.liC;
-        var h3 = newEls.h3;
         var p = newEls.p;
         var liQ = newEls.liQ;
         var liA = newEls.liA;
 
+        // text will be dynamically added from $('.enter-quote').val() and $('add-author').val();
+        liQ.text(qText);
+        liA.text (qAuthor);
+
+        // rating content:
+        var ratingText = ['*','*','*','*','*'].join('   ');
+        p.text(ratingText)
+
         // append title/h3 to container
-        var qContain = liC.append(h3, p);
+        var qContain = liC.append(del, p);
 
         // append li*2 to ul
         var qContent = ul.append(liQ, liA);
@@ -50,6 +59,7 @@
         // append content to container
         var whole = qContain.append(qContent);
 
+        // set equal to undo variable so we can remove it if we want to
         undo = whole;
 
         $('.all-quotes').append(whole)
@@ -58,5 +68,26 @@
     }
 
 
+//============================== Dynamically add quotes ==============================//
+        
+    $('.submit-btn').on('click', function(event) {
 
-// })
+        event.preventDefault();
+
+        var quoteText = $('.enter-quote').val();
+        var quoteAuthor = $('.add-author').val();
+
+        quoteItem(quoteText, quoteAuthor);
+    })
+
+
+//============================== undo last action ==============================//
+
+    $('.undo-btn').on('click', function(event) {
+
+        event.preventDefault();
+        undo.remove();
+    })
+        
+
+})
